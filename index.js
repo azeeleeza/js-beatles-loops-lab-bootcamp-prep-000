569d4e76d8cd6ad662a7f9fe4864e8bf6da8ec5e
LeezaLuncheon Luncheon <leezaluncheon@gmail.com>
2018-03-06 07:05:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians,instruments){
   var beatlesFacts = []
   for (let i = 0; i < musicians.length && i < instruments.length; i++){
-    beatlesFacts += `$(musicians[i]) plays $(instruments[1])`
+    beatlesFacts += `$(musicians[i]) plays $(instruments[i])`
     }
     return beatlesFacts
 
