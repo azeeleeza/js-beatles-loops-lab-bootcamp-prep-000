1631453cbc8a2d6fef44203f76b3af5f18d79c77
LeezaLuncheon Luncheon <leezaluncheon@gmail.com>
2018-03-06 07:18:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function johnLennonFacts(array){
 function iLoveTheBeatles(number){
   var howMuch = []
   do{
-    ++number
     howMuch.push("I love the Beatles!")
+    ++number
   }  while(number < 15)
 }
